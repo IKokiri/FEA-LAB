1c92a9b987f9309805efdbb586bc88b7c1b0c0fd
Luiz Mendes <developer.luizmendes@gmail.com>
2019-06-29 14:42:35 -0300

    FETCH API, LET, TEMPLATE STRING ASYNC AWAIT

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var sucesso = document.getElementById('sucesso');
 var error = document.getElementById('error');
 var latitudeS = document.getElementById('latitude');         
-var longitudeS = document.getElementById('longitude');  
+var longitudeS = document.getElementById('longitude');
+var endereco = document.getElementById('endereco');
 
 document.querySelector('#btn-localizacao').addEventListener('click', function (event) {
     
@@ -13,12 +14,19 @@ document.querySelector('#btn-localizacao').addEventListener('click', function (e
     }
     
 
-    function showPosition(position){
+    async function showPosition(position){
         
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
         sucesso.innerHTML = "Você está:";
-        latitudeS.innerHTML = "Latitude "+position.coords.latitude;
-        longitudeS.innerHTML = "Longitude "+position.coords.longitude;
+        latitudeS.innerHTML = "Latitude "+lat;
+        longitudeS.innerHTML = "Longitude "+lon;
         error.innerHTML = "";
+
+        local = await buscarEndereco(lat,lon)
+        
+        endereco.innerHTML = `Você está proximo de: ${local}`;
     }
 
     function showError(){
@@ -30,10 +38,23 @@ document.querySelector('#btn-localizacao').addEventListener('click', function (e
             sucesso.innerHTML = "";
 
     });
+
+  
           
         
     }
 
+    async function buscarEndereco(lat,lon){
+        local = "";
 
-  
-});
+        await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyBUkoTXt1bL6oLyPUFFdVQXVlJpxbW-jWQ`)
+        .then(function(response){
+            return response.json()
+        }).then(function(data){
+            local = data.results[0].formatted_address;
+        })
+
+        return local;
+    }
+    
+}); 
